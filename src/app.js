c40387036da7e7843aa41a740900af73ee432ec6
Tejas Shastri <tejas.shastri@apptware.com>
2026-05-14 16:12:36 +0530

    minor app.js changes

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5174',
+    origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173',
     credentials: true,
 }));
 app.use(express.json());
